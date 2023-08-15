a058ab68d0209fc0ce850fade146fb2be0bbee2f
Apeksha <apekshawarnakulasooriya98@gmail.com>
2023-08-16 00:45:44 +0530

    Added tabs on the tab bar

--- lib/MainScreen/main_screen.dart
+++ lib/MainScreen/main_screen.dart
@@ -1,3 +1,7 @@
+import 'package:bus_driver/tabPages/earning_tab.dart';
+import 'package:bus_driver/tabPages/home_tab.dart';
+import 'package:bus_driver/tabPages/profile_tab.dart';
+import 'package:bus_driver/tabPages/ratings_tab.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatefulWidget {
@@ -5,7 +9,24 @@ class MainScreen extends StatefulWidget {
   _MainScreenState createState() => _MainScreenState();
 }
 
-class _MainScreenState extends State<MainScreen> {
+class _MainScreenState extends State<MainScreen>
+    with SingleTickerProviderStateMixin {
+  TabController? tabController;
+  int selectedIndex = 0;
+
+  onItemClicked(int index) {
+    setState(() {
+      selectedIndex = index;
+      tabController!.index = selectedIndex;
+    });
+  }
+
+  void initState() {
+    super.initState();
+
+    tabController = TabController(length: 4, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,6 +35,41 @@ class _MainScreenState extends State<MainScreen> {
       ),
       body: TabBarView(
         physics: NeverScrollableScrollPhysics(),
+        controller: tabController,
+        children: const [
+          HomeTabPage(),
+          EarningsTabPage(),
+          RatingsTabPage(),
+          ProfileTabPage(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.credit_card),
+            label: "Earnings",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: "Ratings",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Account",
+          ),
+        ],
+        unselectedItemColor: Colors.white54,
+        selectedItemColor: Colors.white,
+        backgroundColor: Colors.black,
+        type: BottomNavigationBarType.fixed,
+        selectedLabelStyle: TextStyle(fontSize: 14),
+        showUnselectedLabels: true,
+        currentIndex: selectedIndex,
+        onTap: onItemClicked,
       ),
     );
   }
